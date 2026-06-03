35fe630ba6007200df6128dde94af363d1eca140
Fabi <y4wg4s9g46@privaterelay.appleid.com>
2026-06-03 18:10:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ipsc-pwa-v1';
+const CACHE_NAME = 'ipsc-pwa-v2';
 
 // Install-Event: Wird aufgerufen, wenn der Service Worker das erste Mal registriert wird
 self.addEventListener('install', event => {
