cac2553cf25c2a7151aaf186f174673f87683110
Fabi <y4wg4s9g46@privaterelay.appleid.com>
2026-06-16 23:28:57 +0200

    Add files via upload
    Update v24 fixed layout and mobile buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ipsc-pwa-v2';
+const CACHE_NAME = 'ipsc-pwa-v24';
 
 // Install-Event: Wird aufgerufen, wenn der Service Worker das erste Mal registriert wird
 self.addEventListener('install', event => {
