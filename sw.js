0b3e26433a5d8362bdab5e4eb321f7944d230291
Fabi <y4wg4s9g46@privaterelay.appleid.com>
2026-06-17 12:10:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ipsc-pwa-v42';
+const CACHE_NAME = 'ipsc-pwa-v43';
 
 // Install-Event: Wird aufgerufen, wenn der Service Worker das erste Mal registriert wird
 self.addEventListener('install', event => {
