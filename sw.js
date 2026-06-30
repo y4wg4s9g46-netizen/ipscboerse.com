0bd6eaf66790090b5b01517dfe4248ada47c45ce
Fabi <y4wg4s9g46@privaterelay.appleid.com>
2026-06-30 08:04:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ipscboerse-v79bk-analytics-verify-overall-fix';
+const CACHE_NAME = 'ipscboerse-v79bl-verify-header-fix';
 const APP_SHELL_ASSETS = [
     './',
     'index.html',
