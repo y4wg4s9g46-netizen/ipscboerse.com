931adc6c2241bbe3e397139e00ec7f049c014e2c
Fabi <y4wg4s9g46@privaterelay.appleid.com>
2026-06-30 11:23:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ipscboerse-v79bq-analytics-polish';
+const CACHE_NAME = 'ipscboerse-v79br-delete-dedupe';
 const APP_SHELL_ASSETS = [
     './',
     'index.html',
@@ -10,7 +10,7 @@ const APP_SHELL_ASSETS = [
     'header.js?v=77e',
     'auth.js?v=77e',
     'app.js?v=77e',
-    'lang.js?v=79bq',
+    'lang.js?v=79br',
     'marktplatz.html',
     'mein-planer.html',
     'community.html',
@@ -18,7 +18,7 @@ const APP_SHELL_ASSETS = [
     'schiessbuch.html',
     'sg-timer-live.html',
     'tools.html',
-    'analytics.html?v=79bq',
+    'analytics.html?v=79br',
     'wiederladen.html',
     'ipsc-hub.html',
     'doppel-aa.html',
